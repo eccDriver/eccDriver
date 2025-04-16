0153fdb95fbd3b9b1f9026492538b2571458cb0b
eccDriver <1149481468@qq.com>
2025-04-16 22:49:25 +0800

    Update EDG identification.sh

--- EDG identification.sh
+++ EDG identification.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-input_file_a="eccDNA information.bed"
-input_file_b="transcript/enhancer information.bed"
+input_file_a="transcript/enhancer information.bed"
+input_file_b="eccDNA information.bed"
 output_file="eccDNA_contained_transcript/enhancer.bed"
 
-bedtools intersect -a "$input_file_a" -b "$input_file_b" -nonamecheck -wb | \
-awk '$1==$4 && $2<=$5 && $3>=$6' | \
-cut -f 4-6 | \
+bedtools intersect -a "$input_file_a" -b "$input_file_b" -f 1.0 | \
 sort -u > "$output_file"
